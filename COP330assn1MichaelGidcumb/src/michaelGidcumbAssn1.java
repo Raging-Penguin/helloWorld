e73842b7453079531ce0bf99f4e0b404369f5e57
SargeSon <mcgidcumb@gmail.com>
2019-09-05 15:43:51 -0400

    Commit to clone

---
+++ COP330assn1MichaelGidcumb/src/michaelGidcumbAssn1.java
@@ -0,0 +1,9 @@
+//Author: Michael C Gidcumb
+//COP 3330 Programming Assignment 1
+public class michaelGidcumbAssn1 {
+
+	public static void main(String[] args) {
+System.out.println("Hello World!");
+	}
+
+}
